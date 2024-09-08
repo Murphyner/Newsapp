2d16a12539a58834190cf5e2c31d79f76c159996
Agshin <znagshin@gmail.com>
2024-09-08 12:18:55 +0400

    Third

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,14 @@ import { useGetAllCategoriesQuery } from '../store/api'
 
 function Footer() {
     const {data} = useGetAllCategoriesQuery()
+
+    const handleScrollTo = () => {
+        const appHeight = document.querySelector('header')?.offsetHeight || 0;
+        window.scrollTo({
+            top: appHeight,
+            behavior: 'smooth',
+        });
+    };
     return (
         <footer className='bg-black'>
             <div className='container mx-auto '>
@@ -15,10 +23,15 @@ function Footer() {
                     <div className='text-white w-full px-3 lg:px-0  flex flex-col md:flex-row items-center  md:justify-between'>
                         <span className='opacity-50 text-sm'>&#169; Copyright - Newspaper WordPress Theme by TagDiv</span>
                         <ul className='flex justify-between text-sm w-full sm:w-[80%] md:w-[60%] lg:w-[50%] xl:w-[40%] flex-wrap '>
-                            <li className='text-colorCustom2'><Link to={'/'}>Home</Link></li>
-                            <li className='opacity-50 hover:text-colorCustom2 hover:opacity-100 transition-all duration-300'><Link to={'allNews'}>All News</Link></li>
+                            <li onClick={() => {
+                                window.scrollTo({
+                                    top: 0 , 
+                                    behavior: 'smooth'
+                                })
+                            }} className='text-colorCustom2'><Link to={'/'}>Home</Link></li>
+                            <li onClick={handleScrollTo} className='opacity-50 hover:text-colorCustom2 hover:opacity-100 transition-all duration-300'><Link to={'allNews'}>All News</Link></li>
                             {data?.map((item ,i) => {
-                                return <li key={i} className='opacity-50 hover:text-colorCustom2 hover:opacity-100 transition-all duration-300'><Link to={`category/${item._id}`}>{item.name}</Link></li>
+                                return <li onClick={handleScrollTo} key={i} className='opacity-50 hover:text-colorCustom2 hover:opacity-100 transition-all duration-300'><Link to={`category/${item._id}`}>{item.name}</Link></li>
 
                             })}
                         </ul>
