93a2b31a87c65d7518dd6270ad7cf8179ff55d9a
Agshin <znagshin@gmail.com>
2024-09-08 13:17:59 +0400

    login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,51 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import { useLoginPostMutation } from '../store/api'
-
+import React, { useEffect, useState } from 'react';
+import { useLoginPostMutation } from '../store/api';
 
 function Login() {
-    const [login , setLogin] = useState('')
-    const [password , setPassword] = useState('')
-    const [loginPost , {data}] = useLoginPostMutation()
-    
-    
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [loginPost, { data }] = useLoginPostMutation();
+
     useEffect(() => {
         if (data && data.token) {
-            localStorage.setItem("token", data.token)
+            localStorage.setItem("token", data.token);
         }
-    }, [data])
+    }, [data]);
 
-    function handleSubmit(e){
-        e.preventDefault()
-        loginPost({login , password}).then(() => window.location.reload())
+    function handleSubmit(e) {
+        e.preventDefault();
+        loginPost({ login, password }).then(() => window.location.reload());
     }
-  return (
-      <div className='flex items-center h-screen login '>
-          <div className='cart flex bg-white  flex-col justify-center items-center  drop-shadow-sm rounded-md shadow-custom hover:shadow-custom2  transition-all duration-300 p-2 w-[30%] mx-auto gap-5'>
-              <h1 className='font-bold text-xl'>Login</h1>
-              <form className='flex gap-3 flex-col' onSubmit={handleSubmit}>
-                  <input
-                      className='bg-slate-200 p-2 rounded-md placeholder-black placeholder-opacity-50 shadow-custom outline-none transition-all duration-300  focus:shadow-lime-600 shadow-black'
-                      type="text"
-                      placeholder="Username"
-                      value={login}
-                      onChange={(e) => setLogin(e.target.value)}
-                  />
-                  <input
-                      className='bg-slate-200 p-2 rounded-md placeholder-black placeholder-opacity-50 shadow-custom outline-none transition-all duration-300  focus:shadow-lime-600 shadow-black'
-                      type="password"
-                      placeholder="Password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                  />
-                  <button className='bg-lime-400 w-[40%] p-1 mx-auto rounded-xl hover:bg-red-600 transition-all duration-300 hover:rounded-full hover:text-white' type="submit" >
-                      {/* {loading ? 'Logging in...' : 'Login'} */}
-                      Login
-                  </button>
-              </form>
-              {/* {error && <p>{error.error}</p>} */}
-          </div>
-      </div>
-  )
+
+    return (
+        <div className='flex items-center h-screen login bg-gray-100'>
+            <div className='cart flex bg-white flex-col justify-center items-center drop-shadow-sm rounded-md shadow-custom hover:shadow-custom2 transition-all duration-300 p-4 sm:w-[90%] md:w-[60%] lg:w-[40%] xl:w-[30%] mx-auto gap-5'>
+                <h1 className='font-bold text-xl'>Login</h1>
+                <form className='flex gap-4 flex-col w-full' onSubmit={handleSubmit}>
+                    <input
+                        className='bg-slate-200 p-2 rounded-md placeholder-black placeholder-opacity-50 shadow-custom outline-none transition-all duration-300 focus:shadow-lime-600 shadow-black w-full'
+                        type="text"
+                        placeholder="Username"
+                        value={login}
+                        onChange={(e) => setLogin(e.target.value)}
+                    />
+                    <input
+                        className='bg-slate-200 p-2 rounded-md placeholder-black placeholder-opacity-50 shadow-custom outline-none transition-all duration-300 focus:shadow-lime-600 shadow-black w-full'
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button className='bg-lime-400 w-[50%] sm:w-[60%] md:w-[40%] lg:w-[35%] p-2 mx-auto rounded-xl hover:bg-red-600 transition-all duration-300 hover:rounded-full hover:text-white' type="submit">
+                        Login
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
-export default Login
+export default Login;
